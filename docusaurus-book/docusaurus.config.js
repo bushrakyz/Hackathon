877a133ac599cf32ba2e1f2a7dcb8c7df6ec041e
bushrakyz <bushrakyzai.60@gmail.com>
2025-12-25 02:24:27 +0500

    Update docusaurus.config.js

--- docusaurus-book/docusaurus.config.js
+++ docusaurus-book/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-username.github.io', // TODO: Update with actual URL
+  url: 'https://docusaurus-book-gamma.vercel.app', // TODO: Update with actual URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docusaurus-book/', // TODO: Update if using a custom domain
